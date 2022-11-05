94aeceb79a4a3b1713e38c3e1913c89c7f1ca9fa
SCREECH115 <arekw99@gmail.com>
2022-11-05 01:54:38 +0100

    Added logo, line, type of code, click event - toast goes on the middle of the screen and can be deleted by clicking on it, some css imrovments

--- main.js
+++ main.js
@@ -15,31 +15,46 @@ class Toast {
   #location;
   #elem = null;
   #interval = null;
+  #logo;
+  #typeName;
 
-  constructor(handler, content, time, type, location) {
+  constructor(handler, content, time, type, typeName, location, logo) {
     this.#handler = handler;
     this.#content = content;
     this.#time = time;
     this.#startTime = time;
     this.#type = type;
+    this.#typeName = typeName;
     this.#location = location;
+    this.#logo = logo;
   }
 
   render(dom) {
     this.#elem = $.parseHTML(`<div class="notify">
-    <div class="content"><h2><p style="color:red">ALERT</h2></div>
-    <div class="content">CODE: <i>${this.#type}</i></div>
+    <div class="logo"><img src='img/sasp.png'></img></div>
+    <div class="content"><h2><p style="color:#34568B; margin-top: 10px">${
+      this.#typeName
+    }</h2><hr></div>
+    <div class="content">CODE <i>${this.#type}</i></div>
     <div class="content">${this.#location}</div>
-    <div class="content">${hoursAndMinutes}</div>
+    <div class="content">Time ${hoursAndMinutes}</div>
     <div class="time"></div>
     </div>`);
 
     $(this.#elem).click((e) => {
-      $(this.#elem).css("color", "green").css("filter", "brightness(130%)");
+      $(this.#elem).css("filter", "brightness(130%)");
+      $(this.#elem).find("hr").css("border", "1px solid #36aaf9");
+      $(this.#elem).css("border", "1px solid #36aaf9");
+      $(this.#elem).find(".content").css("padding", "1px");
+      $(this.#elem).find(".logo").css("display", "none");
+      $(this.#elem).css("opacity", "0.95").css("animation", "none");
       clearInterval(this.#interval);
-      setTimeout(() => {
-        this.#remove();
-      }, 3000);
+      $(this.#elem).find(".time").css("width", "0%");
+      $(this.#elem).css("left", "-40%");
+      $(this.#elem).off("click");
+      $(this.#elem).click((e) => {
+        $(this.#elem).remove();
+      });
     });
 
     $(dom).append(this.#elem);
@@ -72,18 +87,19 @@ class ToastManager {
   #toastsQueue = [];
   #maxSize;
 
-  constructor(dom = $("#main"), maxSize = 6) {
+  constructor(dom = $("#main"), maxSize = 4) {
     this.#dom = dom;
     this.#maxSize = maxSize;
   }
 
-  addItem(content, time = 1000, type, location) {
+  addItem(content, time = 1000, type, typeName, location) {
     let item = null;
     if (content instanceof Toast) {
       item = content;
     } else {
-      if (!content && !type && !location) throw new Error("No content");
-      item = new Toast(this, content, time, type, location);
+      if (!content && !type && !location && !typeName)
+        throw new Error("No content");
+      item = new Toast(this, content, time, type, typeName, location);
     }
 
     if (this.#toasts.length < this.#maxSize) {
@@ -103,20 +119,20 @@ class ToastManager {
 
 window.onload = () => {
   const toast = new ToastManager();
-  toast.addItem("", 1100, 101, "Magellan Avenue");
+  toast.addItem("", 5000, 101, "Active shooting", "Magellan Avenue");
   setTimeout(() => {
-    toast.addItem("", 1200, 220, "Meteor Street");
+    toast.addItem("", 5000, 501, "Car theft", "Meteor Street");
     setTimeout(() => {
-      toast.addItem("", 1300, 900, "San Andreas Avenue");
+      toast.addItem("", 5000, 900, "Bank robbery", "San Andreas Avenue");
       setTimeout(() => {
-        toast.addItem("", 1400, 960, "Mirror Park Boulevard");
+        toast.addItem("", 5000, 960, "Kidnapping", "Mirror Park Boulevard");
         setTimeout(() => {
-          toast.addItem("", 1500, 501, "Los Santos Airport");
+          toast.addItem("", 5000, 801, "Shop robbery", "Los Santos Airport");
           setTimeout(() => {
-            toast.addItem("", 1600, 609, "Hawick Avenue");
-          }, 2000);
-        }, 2000);
-      }, 2000);
-    }, 2000);
+            toast.addItem("", 5000, 609, "Prostitution", "Hawick Avenue");
+          }, 3000);
+        }, 3000);
+      }, 3000);
+    }, 3000);
   }, 2000);
 };
